6f1b1b0b8f7a29dbe5fd3ae78e1fee062f826e22
Andrea <andrearewards@hotmail.com>
2024-03-31 19:15:16 -0400

    add cross origin to connect to front end react app

--- inventory-management/src/main/java/com/skillstorm/inventorymanagement/controllers/WarehouseController.java
+++ inventory-management/src/main/java/com/skillstorm/inventorymanagement/controllers/WarehouseController.java
@@ -10,6 +10,7 @@ import com.skillstorm.inventorymanagement.services.WarehouseService;
 import org.springframework.http.HttpStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,9 +19,9 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 
-
 @RestController
 @RequestMapping("/warehouses")
+@CrossOrigin("http://localhost:5173")
 public class WarehouseController {
   
   @Autowired
